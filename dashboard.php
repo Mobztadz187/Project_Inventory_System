f8daaa33892ca82d32c4ff7ab9e491962ae7333c
WhtNBlck <jessrey.simeon1@gmail.com>
2025-03-02 10:05:30 +0800

    commit

--- dashboard.php
+++ dashboard.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <link rel="stylesheet" href="CSS/style.css">
-    <script src="https://kit.fontawesome.com/YOUR_FA_KIT.js" crossorigin="anonymous"></script>
     
 </head>
 <body>
@@ -16,10 +15,10 @@
     <div class="dashboard-container">
     <form action="">
         <h3>Dashboard</h3>
-            <div class="total-stocks">Total Stocks</div>
-            <div class="total-students">Total Students</div>
-            <div class="total-borrowed">Total Borrowed</div>
-            <div class="total-returned">Total Returned</div>
+            <div class="total-stocks" style="background-color: skyblue !important;">Total Stocks</div>
+            <div class="total-students" style="background-color: green !important;">Total Students</div>
+            <div class="total-borrowed" style="background-color: orange !important;">Total Borrowed</div>
+            <div class="total-returned" style=" background-color: red !important">Total Returned</div>
     </form>
     </div>
 </main>
